6d7ef07c8a3eaefa1b06516d81ffec3f8e7ec84b
FTC-9929 <tnt9929@hfrobots.com>
2019-05-12 19:56:08 -0500

    Un-disable, a little bit of format cleanup.

--- TeamCode/src/main/java/com/hfrobots/tnt/outreach/paradebot/ParadebotTeleop.java
+++ TeamCode/src/main/java/com/hfrobots/tnt/outreach/paradebot/ParadebotTeleop.java
@@ -43,7 +43,7 @@ import java.util.Iterator;
 import static com.hfrobots.tnt.corelib.Constants.LOG_TAG;
 
 @TeleOp(name="Paradebot Teleop")
-@Disabled
+//@Disabled
 public class ParadebotTeleop extends OpMode
 
 {
@@ -112,11 +112,13 @@ public class ParadebotTeleop extends OpMode
     public void init() {
         Servo flagWaverServo = hardwareMap.servo.get("flagWaverServo");
 
+
         robot = new ParadebotRobot(
                 NinjaMotor.asNeverest20Orbital(hardwareMap.dcMotor.get("leftDriveMotor")),
-                        NinjaMotor.asNeverest20Orbital(hardwareMap.dcMotor.get("rightDriveMotor")),
+                NinjaMotor.asNeverest20Orbital(hardwareMap.dcMotor.get("rightDriveMotor")),
                 flagWaverServo
-                );
+        );
+
         cheesyDrive = new NewCheesyDrive();
 
         setupDriverControls();
